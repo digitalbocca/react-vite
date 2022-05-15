ff419660aac7244f89e0d09f68d60ba1d25ae07a
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-15 10:19:28 -0300

    Altera layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,20 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-column p-0 m-0 min-h-screen">
+      <div className="bg-primary-900 text-white text-4xl font-bold text-center p-4">
+        R$ 50,00
+      </div>
+      <div className='p-4 flex-grow-1'>
+        Formulário
+      </div>
+      <div className="bg-primary-900 text-white pt-4">
+        <i className="pi pi-home"></i>
+        <i className="pi pi-list"></i>
+        <i className="pi pi-plus"></i>
+      </div>
     </div>
   )
 }
