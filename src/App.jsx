0903b221ed30d7d414d22405c6142b61e2b724d7
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-15 14:28:27 -0300

    Adiciona date-picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+import { Calendar } from 'primereact/calendar'
 import './App.css'
 
 function App() {
+  const [date, setDate] = useState({})
+
   return (
     <div className='flex flex-column min-h-screen'>
       <div className='bg-green-700 text-4xl font-bold text-white text-center p-4'>
         R$ 50,00
       </div>
-      <div className='flex-grow-1'>
-        Formulário
+      <div className='flex-grow-1 flex flex-column justify-content-center'>
+        <Calendar
+          className='personal-calendar'
+          showIcon
+          value={date}
+          onChange={(e) => setDate(e.value)}
+          dateFormat="dd/mm/yy"
+        ></Calendar>
       </div>
       <div className='bg-green-700 text-4xl font-bold text-white text-center py-2'>
         <i className="pi pi-home text-4xl p-2 btn-hover border-round-md"></i>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { locale, addLocale } from 'primereact/api'
 import App from './App'
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -7,6 +8,20 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import './index.css'
+
+addLocale("pt-BR", {
+  firstDayOfWeek: 0,
+  dayNames: [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado' ],
+  dayNamesShort: [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab' ],
+  dayNamesMin: [ 'Do', 'Se', 'Te', 'Qu', 'Qu', 'Se', 'Sa' ],
+  monthNames: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+    'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+  monthNamesShort: [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+  today: 'Hoje',
+  clear: 'Limpar'
+});
+
+locale("pt-BR")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
